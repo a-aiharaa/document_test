3833158618df4ed19f16e69c63155e17dfc2a09c
a-aiharaa <a.aihara@cocolofun.co.jp>
2023-06-09 21:31:47 +0900

    add organizationname

--- doc_test/docusaurus.config.js
+++ doc_test/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: '', // Usually your GitHub org/user name.
+  organizationName: 'a-aiharaa', // Usually your GitHub org/user name.
   projectName: 'document_test', // Usually your repo name.
 
   onBrokenLinks: 'throw',
