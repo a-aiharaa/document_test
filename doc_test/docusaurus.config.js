63e54a9de9da50a0c3d86e13c10cf8d459a7c90f
a-aiharaa <a.aihara@cocolofun.co.jp>
2023-06-09 21:51:05 +0900

    baseurlを変更

--- doc_test/docusaurus.config.js
+++ doc_test/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://a-aiharaa.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/document_test',
+  baseUrl: '/document_test/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
